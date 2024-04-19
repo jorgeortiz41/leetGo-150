d0471c04af9038f3a38c5d46a73fc584acb231c1
jorgeortiz41 <jorge.ortiz41@upr.edu>
2024-04-19 17:28:26 -0400

    initialized randomized set file, not implemented

---
+++ randomizedSet.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// 380. Insert Delete GetRandom O(1) (Medium) https://leetcode.com/problems/insert-delete-getrandom-o1/
+// Implement the RandomizedSet class:
+// RandomizedSet() Initializes the RandomizedSet object.
+// bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+// bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+// int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+//
+// You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
+
+type RandomizedSet struct {
+}
+
+func Constructor() RandomizedSet {
+
+}
+
+func (this *RandomizedSet) Insert(val int) bool {
+
+}
+
+func (this *RandomizedSet) Remove(val int) bool {
+
+}
+
+func (this *RandomizedSet) GetRandom() int {
+
+}
+
+func TestRandomizedSet() {
+	fmt.Println("Running RandomizedSet tests")
+	obj := Constructor()
+	fmt.Println("Insert 1:", obj.Insert(1))
+	fmt.Println("Insert 2:", obj.Insert(2))
+	fmt.Println("Insert 3:", obj.Insert(3))
+	fmt.Println("Remove 2:", obj.Remove(2))
+	fmt.Println("GetRandom:", obj.GetRandom())
+	fmt.Println("GetRandom:", obj.GetRandom())
+}
